2f7f0ad05b219acc02df71ba5079ee4a9fe4302c
mezbahalam <mezbahalam26@gmail.com>
2015-06-24 02:36:45 +0600

    create Name class'

---
+++ name.rb
@@ -0,0 +1,24 @@
+class Name
+  def title
+    "Mr. "
+  end
+
+  def first_name
+    "Mezbah"
+  end
+
+  def middle_name
+    'Ul'
+  end
+
+  def last_name
+    'Alam'
+  end
+end
+
+name = Name.new
+puts name # from memory
+puts name.title
+puts name.first_name
+puts name.middle_name
+puts name.last_name
