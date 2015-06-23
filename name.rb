a8f55c0772d959e4aec23cbe30d3841d118b3c66
mezbahalam <mezbahalam26@gmail.com>
2015-06-24 02:58:22 +0600

    add instance variable in  class & use of gets , chomp methods'

--- name.rb
+++ name.rb
@@ -1,24 +1,41 @@
 class Name
+
+  def initialize(title, first_name, middle_name, last_name)
+    @title = title
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
+  end
+
   def title
-    "Mr. "
+    @title
   end
 
   def first_name
-    "Mezbah"
+    @first_name
   end
 
   def middle_name
-    'Ul'
+    @middle_name
   end
 
   def last_name
-    'Alam'
+    @last_name
   end
 end
 
-name = Name.new
+puts 'Enter Your Name Title:'
+title = gets.chomp
+
+puts 'Enter Your Name First Name:'
+first_name = gets.chomp
+
+puts 'Enter Your Name Middle Name:'
+middle_name = gets.chomp
+
+puts 'Enter Your Name Last Name:'
+last_name = gets.chomp
+
+name = Name.new(title, first_name, middle_name, last_name)
 puts name # from memory
-puts name.title
-puts name.first_name
-puts name.middle_name
-puts name.last_name
+puts name.title + " " + name.first_name+ " " + name.middle_name + " " + name.last_name
