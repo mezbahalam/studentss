f1b4435b64cb235a02fe95af23b467ace309721a
mezbahalam <mezbahalam26@gmail.com>
2015-06-24 03:31:08 +0600

    without use attr_writter method'

--- name.rb
+++ name.rb
@@ -1,12 +1,18 @@
 class Name
 
   attr_reader :title, :first_name, :middle_name, :last_name
+  #attr_writer :title, :first_name, :middle_name, :last_name
+  # attr_accessor :title, :first_name, :middle_name, :last_name
 
   def initialize(title, first_name, middle_name, last_name)
     @title = title
     @first_name = first_name
     @middle_name = middle_name
     @last_name = last_name
+  end
+
+  def title=(new_title)
+    @title = new_title
   end
 
 
@@ -25,5 +31,10 @@ puts 'Enter Your Name Last Name:'
 last_name = gets.chomp
 
 name = Name.new(title, first_name, middle_name, last_name)
-puts name.inspect # from memory
+# puts name.inspect # from memory
+puts name.title + " " + name.first_name+ " " + name.middle_name + " " + name.last_name
+
+puts 'Rewrite Your Name Title:'
+name.title = gets.chomp
+puts "Title: #{name.title} "
 puts name.title + " " + name.first_name+ " " + name.middle_name + " " + name.last_name
